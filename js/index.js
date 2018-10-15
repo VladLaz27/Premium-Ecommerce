c84ada4b0cf0f7ad15c82c2d630606edddb42fa5
VladLaz27 <39669227+VladLaz27@users.noreply.github.com>
2018-10-15 13:00:32 +0300

    slider-1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
-    var link = document.querySelector(".show-menu");
 
-    var popup = document.querySelector(".mn-list");
-    var close = popup.querySelector(".close-menu");
 
-    link.addEventListener("click", function (evt) {
-    	popup.classList.add("modal-show");
-  	});
+
+
+
+  	var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });

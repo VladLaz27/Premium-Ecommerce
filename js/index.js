61614cb91181a541e000b393be5bf35d203a08fb
VladLaz27 <39669227+VladLaz27@users.noreply.github.com>
2018-10-16 11:18:44 +0300

    та піхуй як))

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
-
-
-
-
-
-  	var swiper = new Swiper('.swiper-container', {
+	
+	var swiper1 = new Swiper('.swiper1', {
       spaceBetween: 30,
       pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper-pagination1',
         clickable: true,
+      },
+    });
+
+
+ 	
+
+  	var swiper2 = new Swiper('.swiper2', {
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination2',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
     });
